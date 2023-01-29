2eea138517d89cc6711a5f57e5c46aec25eb7da1
Arman <armanmail@gmail.com>
2023-01-29 17:06:16 +0400

    commit1

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aafrikya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 14:27:15 by aafrikya          #+#    #+#             */
+/*   Updated: 2023/01/29 14:27:16 by aafrikya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*replace_str(int fd, char *ptr, char *str);
+char	*new_str(char *str);
+char	*new_ptr(char *str);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strjoin_exlusive(char *str, char *ptr);
+char	*adds(char *ptr);
+size_t	ft_strlen(const char *str);
+int		characters_str(char *str);
+int		characters_ptr(char *str);
+
+#endif
